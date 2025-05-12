bb691c1501f1f136cc738536f154b27783a8b4fb
olilopez5 <olilopez5@outlook.es>
2025-05-12 10:26:02 +0200

    first version

--- HarrisBenedict-iOS/ViewController.swift
+++ HarrisBenedict-iOS/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var weightLabel: UITextField!
     @IBOutlet weak var heightLabel: UITextField!
+    
     @IBOutlet weak var resultLabel: UILabel!
     
     override func viewDidLoad() {
@@ -22,7 +23,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculate(_ sender: Any) {
+       
+            guard let weightText = weightLabel.text,
+                  let heightText = heightLabel.text,
+                  let ageText = ageLabel.text,
+                  let weight = Double(weightText),
+                  let height = Double(heightText),
+                  let age = Double(ageText) else {
+                resultLabel.text = "Introduce datos válidos"
+                return
+            }
+
+            let isMale = genderControl.selectedSegmentIndex == 0 // 0 = Hombre, 1 = Mujer
+            let tmb: Double
+
+            if isMale {
+                tmb = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+            } else {
+                tmb = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
+            }
+
+            resultLabel.text = String(format: "Tu TMB es %.2f kcal/día", tmb)
+        }
+
+        
     }
     
-}
 
